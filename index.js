371e5a3bff47f3a653344830b18d0e6bed3a1f5a
hemantcts <hemantarora.cts@gmail.com>
2024-07-12 12:52:36 +0530

    dffd

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: true
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+    socket.on('join room', (room) => {
+        socket.join(room);
+        console.log(`User joined room ${room}`);
+    });
+
+    socket.on('chat-message', ({ room, message }) => {
+        console.log(message, room)
+        socket.to(room).emit('chat-message', message);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
